77f8db45a73cf6c0ead4338a3d6faadf14553d81
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-23 12:14:04 +0100

    Add console.log as onClick to Button story

--- src/app/components/Button/Button.stories.tsx
+++ src/app/components/Button/Button.stories.tsx
@@ -6,5 +6,9 @@ export default {
   component: Button,
 }
 
-export const Add = (): JSX.Element => <Button>Add concerts</Button>
-export const Save = (): JSX.Element => <Button>Save and go back</Button>
+export const Add = (): JSX.Element => (
+  <Button onClick={() => console.log('Add concert')}>Add concerts</Button>
+)
+export const Save = (): JSX.Element => (
+  <Button onClick={() => console.log('Concert saved')}>Save and go back</Button>
+)
