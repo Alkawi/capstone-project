b733537f3d5a26a50036f5f9795a7d3d7af11684
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-23 14:54:45 +0100

    Add concert type and state

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,23 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
+type Concert = {
+  mainAct: string
+  support?: string
+  concertDate: string
+  location: string
+  numberOfTickets: number
+}
+
 export default function Form(): JSX.Element {
+  const [concert, setConcert] = useState<Concert>({
+    mainAct: '',
+    support: '',
+    concertDate: '',
+    location: '',
+    numberOfTickets: 1,
+  })
+
   return (
     <AddForm action="">
       <Label>
-        Main Act: <Input type="text" required />
+        Main Act:{' '}
+        <Input
+          type="text"
+          required
+          value={concert.mainAct}
+          onChange={(event) =>
+            setConcert({ ...concert, mainAct: event.target.value })
+          }
+        />
       </Label>
       <Label>
-        Support: <Input type="text" />
+        Support:{' '}
+        <Input
+          type="text"
+          value={concert.support}
+          onChange={(event) =>
+            setConcert({ ...concert, support: event.target.value })
+          }
+        />
       </Label>
       <Label>
-        Date: <Input type="date" required />
+        Date:{' '}
+        <Input
+          type="date"
+          required
+          value={concert.concertDate}
+          onChange={(event) =>
+            setConcert({ ...concert, concertDate: event.target.value })
+          }
+        />
       </Label>
       <Label>
-        Location: <Input type="text" required />
+        Location:{' '}
+        <Input
+          type="text"
+          required
+          value={concert.location}
+          onChange={(event) =>
+            setConcert({ ...concert, location: event.target.value })
+          }
+        />
       </Label>
       <Label>
-        Number of tickets: <Input type="number" required />
+        Number of tickets:{' '}
+        <Input
+          type="number"
+          required
+          value={concert.numberOfTickets}
+          onChange={(event) =>
+            setConcert({
+              ...concert,
+              numberOfTickets: event.target.valueAsNumber,
+            })
+          }
+        />
       </Label>
     </AddForm>
   )
