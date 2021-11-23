85e052b753d86cf4e057868a6772130d355230cc
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-23 12:45:45 +0100

    Add more styling

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -5,29 +5,39 @@ export default function Form(): JSX.Element {
   return (
     <AddForm action="">
       <Label>
-        Main Act: <input type="text" required />
+        Main Act: <Input type="text" required />
       </Label>
       <Label>
-        Support: <input type="text" />
+        Support: <Input type="text" />
       </Label>
       <Label>
-        Date: <input type="date" required />
+        Date: <Input type="date" required />
       </Label>
       <Label>
-        Location: <input type="text" required />
+        Location: <Input type="text" required />
       </Label>
       <Label>
-        Number of tickets: <input type="number" required />
+        Number of tickets: <Input type="number" required />
       </Label>
     </AddForm>
   )
 }
 
 const AddForm = styled.form`
+  height: 100vh;
   display: flex;
   flex-direction: column;
+  background-color: var(--color-orange);
+  padding: 20px;
+  color: var(--color-font-dark);
+  font-size: 1.5rem;
 `
 const Label = styled.label`
   display: flex;
   flex-direction: column;
+`
+const Input = styled.input`
+  border: 0;
+  border-radius: 5px;
+  height: 1.5rem;
 `
