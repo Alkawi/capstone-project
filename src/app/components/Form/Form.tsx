f9e26a4223930c62b229e8a494686d17edcbd095
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-23 12:31:12 +0100

    Add labels and inputs

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
 export default function Form(): JSX.Element {
-  return <div></div>
+  return (
+    <form action="">
+      <label>
+        Main Act: <input type="text" required />
+      </label>
+      <label>
+        Support: <input type="text" />
+      </label>
+      <label>
+        Date: <input type="date" required />
+      </label>
+      <label>
+        Location: <input type="text" required />
+      </label>
+      <label>
+        Number of tickets: <input type="number" required />
+      </label>
+    </form>
+  )
 }
