71aa64e39314c4eb5628e68217f8d411bf2f10a0
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-23 14:59:25 +0100

    Refactor button code

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,21 +1,10 @@
-import type { ReactNode } from 'react'
-import React from 'react'
 import styled from 'styled-components'
 
-type ButtonProps = {
-  children: ReactNode
-  onClick: () => void
-}
-function Button({ children, onClick }: ButtonProps): JSX.Element {
-  return <StyledButton onClick={onClick}>{children}</StyledButton>
-}
-
-export default Button
-
-const StyledButton = styled.button`
+const Button = styled.button`
   padding: 10px 20px;
   border-radius: 8px;
   background-color: var(--color-light-orange);
   color: var(--color-font-dark);
   border: 2px solid var(--color-blue);
 `
+export default Button
