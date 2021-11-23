2b2129bb226a9676c2a9d99a083198611c0ea2de
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-23 15:13:07 +0100

    Import and add button to form

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import Button from '../Button/Button'
 
 type Concert = {
   mainAct: string
@@ -77,6 +78,7 @@ export default function Form(): JSX.Element {
           }
         />
       </Label>
+      <Button>Save and go back</Button>
     </AddForm>
   )
 }
