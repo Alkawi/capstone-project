aa1e4e1a3d7b73ad43bc3dcd356e087e80b27534
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-24 16:07:24 +0100

    Implement dashboard

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './Pages/Dashboard'
 
 function App(): JSX.Element {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/add">
-          <main>Add</main>
-        </Route>
-        <Route path="/">
-          <main>Home</main>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <Dashboard />
 }
 
 export default App
