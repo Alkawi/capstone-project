275b9016883836e14081e96626239d0812f37d5e
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-24 16:09:34 +0100

    Implement Form and Add Button. Add logic

--- src/app/Pages/Dashboard.tsx
+++ src/app/Pages/Dashboard.tsx
@@ -1,5 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../components/Button/Button'
+import Form from '../components/Form/Form'
+
+type Concert = {
+  mainAct: string
+  support?: string
+  concertDate: string
+  location: string
+  numberOfTickets: number
+}
 
 export default function Dashboard(): JSX.Element {
-  return <></>
+  const [showForm, setShowForm] = useState<boolean>(false)
+  const [concerts, setConcerts] = useState<Concert[]>([])
+
+  function handleSubmit(concert: Concert): void {
+    const newConcert = concert
+
+    setConcerts([...concerts, newConcert])
+    setShowForm(false)
+  }
+
+  function handleClick(): void {
+    setShowForm(true)
+  }
+
+  return (
+    <div>
+      <main>
+        <Button onClick={handleClick}>Add concert</Button>
+      </main>
+      <main>{showForm && <Form onSubmit={handleSubmit} />}</main>
+    </div>
+  )
 }
