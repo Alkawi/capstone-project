69edcb49649fe60393952f1eef13daf31513de97
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-25 10:48:54 +0100

    Implement logic for upcoming component

--- src/app/components/UpcomingConcerts/UpcomingConcerts.tsx
+++ src/app/components/UpcomingConcerts/UpcomingConcerts.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import moment from 'moment'
 
-export default function UpcomingConcerts(): JSX.Element {
-  return <div></div>
+type Concert = {
+  id: string
+  mainAct: string
+  support?: string
+  concertDate: Date
+  numberOfTickets: number
+}
+
+type UpcomingConcertsProps = {
+  concerts: Concert[]
+}
+export default function UpcomingConcerts({
+  concerts,
+}: UpcomingConcertsProps): JSX.Element {
+  const numberOfUpcomingConcerts = concerts.filter((concert) =>
+    moment(concert.concertDate).isSameOrAfter(moment())
+  ).length
+
+  return <span>Upcoming concerts: {numberOfUpcomingConcerts}</span>
 }
