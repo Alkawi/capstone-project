e09cc3b1e6b79919026bdc4d55a4e2788d99d5fa
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-29 15:23:46 +0100

    Create basic form

--- src/app/components/UserForm/UserForm.tsx
+++ src/app/components/UserForm/UserForm.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
 export default function UserForm(): JSX.Element {
-  return <div></div>
+  return (
+    <form>
+      <label>
+        Username: <input type="text" minLength={5} maxLength={25} required />
+      </label>
+      <label>
+        Password:{' '}
+        <input type="password" minLength={8} maxLength={64} required />
+      </label>
+    </form>
+  )
 }
