5ae64436b06a57a196a7045692a421cd47ab9574
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-30 15:21:24 +0100

    Extract Update Password Form to its own component

--- src/app/components/UserForm/UserForm.tsx
+++ src/app/components/UserForm/UserForm.tsx
@@ -3,7 +3,7 @@ import Button from '../Button/Button'
 import styled from 'styled-components'
 
 type UserFormProps = {
-  formType: 'Login' | 'Register' | 'Update'
+  formType: 'Login' | 'Register'
   onSubmit: (username: string, password: string, newPassword?: string) => void
 }
 
@@ -13,68 +13,39 @@ export default function UserForm({
 }: UserFormProps): JSX.Element {
   const [username, setUsername] = useState<string>('')
   const [password, setPassword] = useState<string>('')
-  const [newPassword, setNewPassword] = useState<string>('')
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
     event.preventDefault()
-    formType !== 'Update'
-      ? onSubmit(username, password)
-      : onSubmit(username, password, newPassword)
+
+    onSubmit(username, password)
   }
 
   return (
     <FormContainer onSubmit={(event) => handleSubmit(event)}>
-      {formType !== 'Update' ? (
-        <InputContainer>
-          <Label>
-            Username:
-            <Input
-              type="text"
-              minLength={5}
-              maxLength={25}
-              required
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
-            />
-          </Label>
-          <Label>
-            Password:
-            <Input
-              type="password"
-              minLength={8}
-              maxLength={64}
-              required
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </Label>
-        </InputContainer>
-      ) : (
-        <InputContainer>
-          <Label>
-            Old password:
-            <Input
-              type="password"
-              minLength={8}
-              maxLength={64}
-              required
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </Label>
-          <Label>
-            New password:
-            <Input
-              type="password"
-              minLength={8}
-              maxLength={64}
-              required
-              value={newPassword}
-              onChange={(event) => setNewPassword(event.target.value)}
-            />
-          </Label>
-        </InputContainer>
-      )}
+      <InputContainer>
+        <Label>
+          Username:
+          <Input
+            type="text"
+            minLength={5}
+            maxLength={25}
+            required
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </Label>
+        <Label>
+          Password:
+          <Input
+            type="password"
+            minLength={8}
+            maxLength={64}
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Label>
+      </InputContainer>
       <Button>{formType}</Button>
     </FormContainer>
   )
