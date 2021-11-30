f83dc754395a64a976a8152d4c12cc648c866506
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-30 15:25:19 +0100

    Correct UserForm story

--- src/app/components/UserForm/UserForm.stories.tsx
+++ src/app/components/UserForm/UserForm.stories.tsx
@@ -14,19 +14,11 @@ function handleRegisterSubmit(username: string, password: string): void {
   console.log(`Registered ${username}, ${password}`)
 }
 
-function handleUpdateSubmit(
-  username: string,
-  password: string,
-  newPassword?: string
-): void {
-  console.log(`Update ${username}s PW from ${password} to ${newPassword}`)
-}
+
 export const UserFormLogin = (): JSX.Element => (
   <UserForm formType="Login" onSubmit={handleLoginSubmit} />
 )
 export const UserFormRegister = (): JSX.Element => (
   <UserForm formType="Register" onSubmit={handleRegisterSubmit} />
 )
-export const UserFormUpdate = (): JSX.Element => (
-  <UserForm formType="Update" onSubmit={handleUpdateSubmit} />
-)
+
