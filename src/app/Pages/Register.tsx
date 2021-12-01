410204b7b6cba0ca9af3161b3b5e4c98a576c512
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-01 13:12:38 +0100

    Add styling to Login Link

--- src/app/Pages/Register.tsx
+++ src/app/Pages/Register.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserForm from '../components/UserForm/UserForm'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 export default function Register(): JSX.Element {
   function handleSubmit(username: string, password: string) {
@@ -8,10 +9,13 @@ export default function Register(): JSX.Element {
   }
 
   return (
-    <LoginContainer>
+    <RegistrationContainer>
       <Heading>Registration</Heading>
       <UserForm formType="Register" onSubmit={handleSubmit} />
-    </LoginContainer>
+      <LinkContainer>
+        <Link to="login">Log in</Link> to an existing account
+      </LinkContainer>
+    </RegistrationContainer>
   )
 }
 
@@ -19,8 +23,13 @@ const Heading = styled.h1`
   color: var(--color-font-dark);
   align-self: center;
 `
-const LoginContainer = styled.div`
+const RegistrationContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 1rem;
+`
+
+const LinkContainer = styled.span`
+  color: var(--color-font-dark);
+  align-self: center;
 `
