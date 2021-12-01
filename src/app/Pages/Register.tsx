31df5167359691b1048d1c292468aa8f7a82ed68
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-01 13:04:07 +0100

    Add UserForm

--- src/app/Pages/Register.tsx
+++ src/app/Pages/Register.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import UserForm from '../components/UserForm/UserForm'
 
 export default function Register(): JSX.Element {
   return (
     <div>
       <h1>Registration</h1>
+      <UserForm formType="Register" onSubmit={handleSubmit} />
     </div>
   )
 }
