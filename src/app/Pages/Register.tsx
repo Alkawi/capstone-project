ad093d3b9bdf9b28d74fac319303b248afd1ac65
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-01 13:07:16 +0100

    Add styling

--- src/app/Pages/Register.tsx
+++ src/app/Pages/Register.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserForm from '../components/UserForm/UserForm'
+import styled from 'styled-components'
 
 export default function Register(): JSX.Element {
   function handleSubmit(username: string, password: string) {
@@ -7,9 +8,19 @@ export default function Register(): JSX.Element {
   }
 
   return (
-    <div>
-      <h1>Registration</h1>
+    <LoginContainer>
+      <Heading>Registration</Heading>
       <UserForm formType="Register" onSubmit={handleSubmit} />
-    </div>
+    </LoginContainer>
   )
 }
+
+const Heading = styled.h1`
+  color: var(--color-font-dark);
+  align-self: center;
+`
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`
