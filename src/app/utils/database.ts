18fe16b3b7bcf3bade40babdd19f280de78265c9
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-01 13:36:13 +0100

    Create getUserCollection function

--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -1,6 +1,13 @@
+import type { Collection } from 'mongodb'
 import { MongoClient } from 'mongodb'
 
+let client: MongoClient
+
 export async function connectDatabase(url: string): Promise<void> {
-  const client: MongoClient = new MongoClient(url)
+  client = new MongoClient(url)
   await client.connect()
+}
+
+export function getUserCollection(): Collection<Document> {
+  return client.db().collection('users')
 }
