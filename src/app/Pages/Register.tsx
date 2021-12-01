e7b46bcf0195bff229f9a954a8ac35e28d5fd0dc
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-01 13:06:15 +0100

    Add handleSubmit function with test console.log

--- src/app/Pages/Register.tsx
+++ src/app/Pages/Register.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import UserForm from '../components/UserForm/UserForm'
 
 export default function Register(): JSX.Element {
+  function handleSubmit(username: string, password: string) {
+    console.log(`Registered ${username} with pw ${password}`)
+  }
+
   return (
     <div>
       <h1>Registration</h1>
