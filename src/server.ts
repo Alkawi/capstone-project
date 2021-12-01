db5f9229899270ff48a8e7130deadc219e2a5914
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-01 13:46:01 +0100

    Import connectDatabase & getUserCollection

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express from 'express'
+import { connectDatabase, getUserCollection } from './app/utils/database'
 
 const app = express()
 const port = process.env.PORT || 3001
