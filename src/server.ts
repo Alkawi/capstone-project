dd70a5e69c22be4ced541a225a5763ab766558fd
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-01 13:52:41 +0100

    Add check for MONGODB_URI

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ dotenv.config()
 import express from 'express'
 import { connectDatabase, getUserCollection } from './app/utils/database'
 
+if (!process.env.MONGODB_URI) {
+  throw new Error("Couldn't connect to the database")
+}
+
 const app = express()
 const port = process.env.PORT || 3001
 
