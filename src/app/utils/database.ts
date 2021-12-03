b1ba40a5398c47b67b7452378e5ca09bac54faca
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-03 10:56:12 +0100

    Add simple user schema

--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -3,11 +3,26 @@ import { MongoClient } from 'mongodb'
 
 let client: MongoClient
 
+interface Concerts {
+  id: string
+  mainAct: string
+  support?: string
+  concertDate: string
+  location: string
+  numberOfTickets: number
+}
+
+interface User {
+  username: string
+  password: string
+  concerts?: Concerts[]
+}
+
 export async function connectDatabase(url: string): Promise<void> {
   client = new MongoClient(url)
   await client.connect()
 }
 
-export function getUserCollection(): Collection<Document> {
+export function getUserCollection(): Collection<User> {
   return client.db().collection('users')
 }
