36cc016d91573fc4b9c906a486bce47a2a0fb8d8
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-07 14:21:00 +0100

    Add simple display of total number of concerts

--- src/app/Pages/UserProfile.tsx
+++ src/app/Pages/UserProfile.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { useParams } from 'react-router'
 import UpdatePasswordForm from '../components/UpdatePasswordForm/UpdatePasswordForm'
 import styled from 'styled-components'
+import useFetch from '../hooks/useFetch'
+import type { Concert } from '../types'
 
 export default function UserProfile(): JSX.Element {
   const { username } = useParams()
+  const concerts = useFetch<Concert[]>(`/${username}/concerts`)
 
   async function handleSubmit(password: string, newPassword: string) {
     const response = await fetch(`/${username}/changePassword`, {
@@ -26,6 +29,9 @@ export default function UserProfile(): JSX.Element {
     <PageContainer>
       <Heading>Profile</Heading>
       <UpdatePasswordForm onSubmit={handleSubmit} />
+      <TotalConcerts>
+        Total concerts: {concerts ? concerts.length : 0}
+      </TotalConcerts>
     </PageContainer>
   )
 }
@@ -35,5 +41,9 @@ const PageContainer = styled.div`
   display: grid;
 `
 const Heading = styled.h1`
+  justify-self: center;
+`
+const TotalConcerts = styled.span`
+  color: var(--color-font-dark);
   justify-self: center;
 `
