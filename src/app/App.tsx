c19df4012d47c8c12068cc115e352a2aa57ae554
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-08 14:05:41 +0100

    Implement logo as header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,12 @@ import AddConcerts from './Pages/AddConcerts'
 import Dashboard from './Pages/Dashboard'
 import Login from './Pages/Login'
 import Register from './Pages/Register'
+import logo from '../images/logo.gif'
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
+      <img src={logo} width="100%" />
       <Routes>
         <Route path="/">
           <Route path=":username" element={<Dashboard />} />
