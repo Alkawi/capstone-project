8ae9581ef20982081c1365504a99755ba242143b
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-16 17:30:38 +0100

    Implement navigation component

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,53 +1,58 @@
 import React from 'react'
 import { HiHome, HiPlusCircle, HiOutlineUser } from 'react-icons/hi'
-import { NavLink, useParams } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 
-type NavigationProps = {
-  page: 'dashboard' | 'add' | 'profile'
+type NavProps = {
+  pathName: string
 }
 
-export default function Navigation({ page }: NavigationProps): JSX.Element {
-  const { username } = useParams()
+export default function Navigation(): JSX.Element {
+  const { pathname } = useLocation()
+  let username = pathname
+  if (pathname.lastIndexOf('/') !== 0) {
+    username = pathname.substring(0, pathname.indexOf('/', 1))
+  }
 
   return (
-    <NavBar>
-      <NavLink to={`/${username}`}>
-        <HomeIcon page={page} />
+    <NavBar pathName={pathname}>
+      <NavLink to={`${username}`}>
+        <HomeIcon />
       </NavLink>
-      <NavLink to={`/${username}/addConcert`}>
-        <AddIcon page={page} />
+      <NavLink to={`${username}/addConcert`}>
+        <AddIcon />
       </NavLink>
-      <NavLink to={`/${username}/profile`}>
-        <ProfileIcon page={page} />
+      <NavLink to={`${username}/profile`}>
+        <ProfileIcon />
       </NavLink>
     </NavBar>
   )
 }
 
-const NavBar = styled.nav`
+const NavBar = styled.nav<NavProps>`
   border: 1px solid var(--color-font-dark);
-  display: flex;
+  position: sticky;
+  bottom: 0;
+  background-color: var(--color-orange);
+  display: ${(props) =>
+    props.pathName === '/login' || props.pathName === '/register'
+      ? 'none'
+      : 'flex'};
   justify-content: space-around;
   padding: 1rem;
 `
-const HomeIcon = styled(HiHome)<NavigationProps>`
-  color: ${(props) =>
-    props.page === 'dashboard'
-      ? 'var(--color-blue)'
-      : 'var(--color-font-dark)'};
+const HomeIcon = styled(HiHome)`
+  color: var(--color-font-dark);
   height: 1.5rem;
   width: 1.5rem;
 `
-const AddIcon = styled(HiPlusCircle)<NavigationProps>`
-  color: ${(props) =>
-    props.page === 'add' ? 'var(--color-blue)' : 'var(--color-font-dark)'};
+const AddIcon = styled(HiPlusCircle)`
+  color: var(--color-font-dark);
   height: 1.5rem;
   width: 1.5rem;
 `
-const ProfileIcon = styled(HiOutlineUser)<NavigationProps>`
-  color: ${(props) =>
-    props.page === 'profile' ? 'var(--color-blue)' : 'var(--color-font-dark)'};
+const ProfileIcon = styled(HiOutlineUser)`
+  color: var(--color-font-dark);
   height: 1.5rem;
   width: 1.5rem;
 `
