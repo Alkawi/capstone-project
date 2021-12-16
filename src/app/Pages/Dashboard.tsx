e40d247b3fa10106d8ba47c1a94acfda1a8ab361
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-12-16 17:23:11 +0100

    Remove Link to Add Form

--- src/app/Pages/Dashboard.tsx
+++ src/app/Pages/Dashboard.tsx
@@ -3,69 +3,47 @@ import Card from '../components/Card/Card'
 import styled from 'styled-components'
 import UpcomingConcerts from '../components/UpcomingConcerts/UpcomingConcerts'
 import type { Concert } from '../types'
-import Button from '../components/Button/Button'
-import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import useFetch from '../hooks/useFetch'
 
 export default function Dashboard(): JSX.Element {
   const { username } = useParams()
-  const navigate = useNavigate()
   const [concerts] = useFetch<Concert[]>(`/api/${username}/concerts`)
 
-  async function handleClick() {
-    await fetch('/api/logout', {
-      method: 'POST',
-    })
-    navigate('/login')
-  }
-
   return (
-    <div>
-      <Container>
-        <Header>
-          <UpcomingConcerts concerts={concerts} />
-          <Link to={`/${username}/addConcert`}>
-            <Button>Add concert</Button>
-          </Link>
-        </Header>
-        <CardContainer>
-          {concerts &&
-            concerts.map((concert) => (
-              <Card key={concert.id} concert={concert} />
-            ))}
-          {!concerts && (
-            <MissingConcerts>
-              <h2>No concerts available</h2>
-              <p>Please add a concert</p>
-            </MissingConcerts>
-          )}
-        </CardContainer>
-        <Button onClick={() => handleClick()}>Logout</Button>
-      </Container>
-    </div>
+    <Container>
+      <UpcomingConcerts concerts={concerts} />
+      <CardContainer>
+        {concerts &&
+          concerts.map((concert) => (
+            <Card key={concert.id} concert={concert} />
+          ))}
+        {!concerts && (
+          <MissingConcerts>
+            <h2>No concerts available</h2>
+            <p>Please add a concert</p>
+          </MissingConcerts>
+        )}
+      </CardContainer>
+    </Container>
   )
 }
 
-const Header = styled.header`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`
 const Container = styled.main`
   display: grid;
   gap: 1rem;
+  overflow-y: auto;
   grid-template-rows: auto 1fr;
-  padding: 1rem;
 `
 const MissingConcerts = styled.article`
   align-self: center;
   border-radius: 8px;
   padding: 1rem;
+  margin: 0 1rem;
   border: 1px solid var(--color-font-dark);
   background-color: var(--color-light-orange);
 `
 const CardContainer = styled.div`
   display: grid;
-  overflow-y: auto;
   gap: 1rem;
 `
